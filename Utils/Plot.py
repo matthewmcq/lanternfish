12c51d8ca884094bd06325320896dc1b02d0b799
Rayhan Meghji <90580552+rmeghji@users.noreply.github.com>
2024-05-10 03:18:18 -0400

    Update Plot.py

--- Utils/Plot.py
+++ Utils/Plot.py
@@ -1,40 +1,53 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import librosa
+import Config as cfg
 
+def display_waveforms(predict_true_0, predict_train_0, i):
+    model_config = cfg.cfg()
+    # Prepare the time axis
+    time = np.arange(sum([len(coef) for coef in predict_true_0]))
 
+    plt.rcParams['figure.figsize'] = (8, 8)
+    plt.rcParams['font.size'] = 8
 
-def visualize_wavelet(coeffs):
-    levels = coeffs.shape[0]
-    channels = 1
-    coeffs = coeffs.numpy()  # Convert TensorFlow tensor to NumPy array
-    duration = 4  # Duration of the audio snippet in seconds
+    # Plot the wavelet coefficients
+    fig, ax = plt.subplots(model_config['wavelet_depth'] + 1, 1, figsize=(12, 6))
 
-    fig, ax = plt.subplots(figsize=(50, 15))
-    for i in range(levels):
-    
-        ci = np.abs(coeffs[i, :])
-        # max_val = np.max(ci)
-        # if max_val == 0:
-        #     max_val = 1
-        # ci /= max_val
-        # vmin = np.min(ci)
-        # vmax = np.max(ci)
-        ax.imshow(np.sqrt(ci.reshape(1, -1)), cmap='inferno',  aspect='auto',
-                extent=[0, duration, i * channels, i * channels + 1], interpolation='nearest')
+    # Plot the original signal
+    start = 0
+    for level, coef in enumerate(predict_true_0):
+        ax[0].plot(time[start:start+len(coef)], coef, label=f'Level {level+1}')
+        start += len(coef)
+    ax[0].set_title('Original Signal')
+    ax[0].set_xlabel('Time')
+    ax[0].set_ylabel('Amplitude')
+    ax[0].legend()
 
-    ax.set_title("Wavelet Coefficients")
-    ax.set_ylabel("Level and Channel")
+    # Plot the wavelet coefficients
+    start = 0
+    for level in range(model_config['wavelet_depth']):
+        ax[level + 1].plot(time[start:start+len(predict_true_0[-level])], predict_true_0[-level], label='True')
+        ax[level + 1].plot(time[start:start+len(predict_train_0[-level])], predict_train_0[-level], label='Predicted')
+        ax[level + 1].set_title(f'Wavelet Level {level + 1}')
+        ax[level + 1].set_xlabel('Time')
+        ax[level + 1].set_ylabel(f'Coef Level {level + 1}')
+        ax[level + 1].legend()
+        start += len(predict_true_0[-level])
 
-    level_labels = [f"Level {levels - i}" for i in range(levels)]
-    channel_labels = [f"Channel {ch + 1}" for ch in range(channels)]
-    level_channel_labels = [f"{level} {channel}" for level in level_labels for channel in channel_labels]
-
-    ax.set_yticks(np.arange(0.5, levels * channels, 1))
-    ax.set_yticklabels(level_channel_labels)
-    ax.set_xlim([0, duration])
-    ax.set_ylim([0, levels * channels])
-    ax.set_xlabel("Time (s)")
-
-    plt.colorbar(ax.imshow(coeffs[0, :].reshape(1, -1), aspect='auto'), ax=ax, label="Coefficient Magnitude")
     plt.tight_layout()
-    plt.show()
+    # plt.show()
+    plt.savefig(f'audiofiles/sample{i}/waveforms.png')
+    plt.close(fig)
+    
+
+def display_stft(coeffs, i, name):
+    # Display the STFT of the predict_train_a3 audio
+    fig, ax = plt.subplots(figsize=(6, 6))
+    stft = librosa.stft(coeffs, n_fft=2048, hop_length=256)
+    ax.pcolormesh(librosa.amplitude_to_db(np.abs(stft), ref=np.max), cmap='viridis')
+    ax.set_title(f'STFT of predict_train_{name}, sample{i}')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Frequency')
+    plt.savefig(f'audiofiles/sample{i}/{name}_stft.png')
+    plt.close(fig)
