fa91ac1a54797b0cb3886208e742e7c5e96aad89
Matthew McQuistion <97474754+matthewmcq@users.noreply.github.com>
2024-05-10 01:23:11 -0400

    Update batch_data.py

--- Utils/Batch/batch_data.py
+++ Utils/Batch/batch_data.py
@@ -26,10 +26,12 @@ def generate_pairs(path_to_song: str, stem_type: str, level: int =12, max_songs_
     - tuple, pair of wavelet data for training and true data for the audio file
     '''
 
-    ## TODO make random list of indices to select from that are of length max_songs_per_stem
+    
 
     len_train = len(os.listdir(path_to_song + 'y_train/'))
 
+    # get the indices for the training data, if max_songs_per_stem is greater than the number of songs, set indices to None
+    # This is to prevent the error that occurs when replace=False
     if max_songs_per_stem >= len_train:
         indices=None
     else:
@@ -81,21 +83,30 @@ def make_test_set(train_dict: dict, true_dict: dict, stem_type: str, path_to_son
         
         # get the true key
         train = train_dict[key]
+        
+        ## get the index of the key in form vocals_<perm number>_<song index>.wav
+        ## Want: vocals_<song index>.wav from y_true
         index = int(key.split("_")[-1].split(".")[0])
+        
+        # Check to ensure index is valid
         assert int(index) >= 0
         assert index != ""
+        
+        # Get true key, which is just the filepath
         true_key = f"{stem_type}_{index}.wav"
         true_key = path_to_song + "y_true/" + true_key
         
         # get the wavelet transform for the training and true data
-        if diff:
+        if diff: # diff for training model to learn the difference
+                 # i.e. subtract vocals from full mix -- does not work as well as expected
             Wavelets.getWaveletTransformDiff(train_dict, true_dict, key, true_key, level)
         else:
+            # normal case, get the wavelet transforms of full mix and just the stem
             Wavelets.getWaveletTransform(train_dict, key, level)
             Wavelets.getWaveletTransform(true_dict, true_key, level)
 
 
-        # get the wavelet coefficients shapes
+        # get the wavelet coefficients shape -- should be the same for all samples
         if train_shape is None:
             train_shape = [c.shape for c in train.dwt]
 
@@ -105,23 +116,11 @@ def make_test_set(train_dict: dict, true_dict: dict, stem_type: str, path_to_son
         train_tensor = train.tensor_coeffs
         true_tensor = true.tensor_coeffs
 
-        vox_isolated = train_tensor - true_tensor
-        
-        
-        
-        if np.all(vox_isolated == 0) or np.all(train_tensor == 0) or np.all(true_tensor == 0):
-            continue  # skip this sample
-        if train_tensor.shape[0] != true_tensor.shape[0]:
-            continue
-        
-
         # add to the list
         y_train.append(train_tensor)
         y_true.append(true_tensor)
 
     return y_train, y_true, train_shape 
-
-
 
 
 
@@ -140,7 +139,6 @@ def batch_wavelets_dataset(path_to_training: str, stem_type: str, level: int =12
     return: 
     - tf.data.Dataset, batched wavelet data
     '''
-    
     
 
     # find all songs in the training data
@@ -161,8 +159,6 @@ def batch_wavelets_dataset(path_to_training: str, stem_type: str, level: int =12
         song_name = song.split('/')[-1]
         if song_name in BLEED_SONGS:
             continue
-        
-        
         
         
         # generate pairs for each song
@@ -204,47 +200,42 @@ def batch_wavelets_dataset(path_to_training: str, stem_type: str, level: int =12
 
     y_train = np.concatenate(y_train)
     y_true = np.concatenate(y_true)
-    # print(f"y_train shape: {y_train.shape}")
-    # print(f"y_true shape: {y_train.shape}")
-    # convert to tensors
     
-    # y_train = tf.convert_to_tensor(y_train)
-    # y_true = tf.convert_to_tensor(y_true)
-    # print(f"y_train shape: {y_train.shape}")
     y_train, y_true = downcoef_audio(y_train, y_true, shape, level)
-    # print(f"y_train shape: {y_train.shape}")
-    return y_train, y_true, shape
+    
+    
+    # UNCOMMENT THIS LINE AND COMMENT THE LINE BELOW TO RETURN THE TENSORS FOR DEBUGGING
+    # return y_train, y_true, shape 
     
     # Convert to TensorFlow dataset
-    # dataset = tf.data.Dataset.from_tensor_slices((y_train, y_true))
-    # print(dataset.element_spec)
+    dataset = tf.data.Dataset.from_tensor_slices((y_train, y_true))
+    print(dataset.element_spec)
     
-    # if training:
-    #     # Shuffle the dataset
-    #     dataset = dataset.shuffle(buffer_size=len(dataset))
+    if training:
+        # Shuffle the dataset
+        dataset = dataset.shuffle(buffer_size=len(dataset))
 
-    # # Split the dataset into training and validation sets
-    # validation_size = int(len(dataset) * validation_split)
-    # train_dataset = dataset.skip(validation_size)
-    # val_dataset = dataset.take(validation_size)
+    # Split the dataset into training and validation sets
+    validation_size = int(len(dataset) * validation_split)
+    train_dataset = dataset.skip(validation_size)
+    val_dataset = dataset.take(validation_size)
 
-    # print(train_dataset.element_spec)
-    # print(val_dataset.element_spec)
-    # # Batch the datasets
-    # train_dataset = train_dataset.batch(batch_size)
-    # val_dataset = val_dataset.batch(batch_size)
+    print(train_dataset.element_spec)
+    print(val_dataset.element_spec)
+    # Batch the datasets
+    train_dataset = train_dataset.batch(batch_size)
+    val_dataset = val_dataset.batch(batch_size)
 
-    # # Prefetch the datasets for better performance
-    # train_dataset = train_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
-    # val_dataset = val_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+    # Prefetch the datasets for better performance
+    train_dataset = train_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+    val_dataset = val_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
 
 
-    # return train_dataset, val_dataset, shape
+    return train_dataset, val_dataset, shape
 
 def downcoef_audio(y_train, y_true, shape, level, wavelet='haar'):
     
     # y_train and true are tensors, [:,:,0] is the approximation coefficients, [:,:,i] are the detail coefficients
-    
     
     def inverseWaveletReshape(tensor_coeffs, shape, wavelet_depth, flag=False):
         
@@ -259,27 +250,32 @@ def downcoef_audio(y_train, y_true, shape, level, wavelet='haar'):
             # Get the coefficients for the current level
             level_coeffs = coeffs[:, :, level]
             
+            # get the correct shape
             dsize = (shape[level][0], 1)
-            # print(f"dsize: {dsize}")
+            
+            # resize the coefficients to match the original shape
             reshaped_coeffs = cv2.resize(level_coeffs.reshape(1, -1), dsize=dsize, interpolation=cv2.INTER_NEAREST_EXACT).flatten()
 
+            # add to the list
             downscaled_coeffs.append(reshaped_coeffs)
 
+        # return the downscaled coefficients
         return downscaled_coeffs
         
     
     def convert_to_audio_tensor(downscaled_coeffs):
         audio_tensor = []
-        # print(f"full_array len: {len(downscaled_coeffs)}")
+        
+        # for each level, get the coefficients and convert directly to sub-band audio
         for i in range(len(downscaled_coeffs)):
-            part = 'd' # default to details
+            part = 'd' # default to details coefficients
             coeffs = downscaled_coeffs[i]
-            # print(f"coeffs shape: {len(coeffs)}")
+            
             if i == 0:
-                part = 'a'
-                audio_tensor.append(pywt.upcoef(part, coeffs, wavelet, level=level))
+                part = 'a' # approximation coefficients
+                audio_tensor.append(pywt.upcoef(part, coeffs, wavelet, level=level)) # level=level because we are at the top level
             elif i == 1:
-                audio_tensor.append(pywt.upcoef(part, coeffs, wavelet, level=level))
+                audio_tensor.append(pywt.upcoef(part, coeffs, wavelet, level=level)) # level=level because we are at the top level of details
             else:
                 audio_tensor.append(pywt.upcoef(part, coeffs, wavelet, level=(level-(i - 1))))
         # print(f"audio_tensor shape: {[a.shape for a in audio_tensor]}")
@@ -306,6 +302,8 @@ def downcoef_audio(y_train, y_true, shape, level, wavelet='haar'):
     y_train_audio = process_arrray(y_train)
     y_true_audio = process_arrray(y_true)
     
+    
+    ## UNCOMMENT FOR DEBUGGING: WRITE TO AUDIO FILES
     # def write_to_audiofile(audio_tensor, name):
     #     audio_sum = 0
     #     for i in range(len(audio_tensor)):
@@ -332,3 +330,89 @@ def downcoef_audio(y_train, y_true, shape, level, wavelet='haar'):
     # print(f"y_true_audio shape: {y_true_audio.shape}")
     
     return y_train_audio, y_true_audio
+
+
+
+
+def batch_wavelets_debug(path_to_training: str, stem_type: str, level: int =12, batch_size: int =8, max_songs: int =2, max_samples_per_song: int =10, num_features: int = 65536, validation_split=0.2, training=True, diff=False) -> tf.data.Dataset:
+    '''
+    Batch the wavelet data for training
+
+    params:
+    - path_to_training: str, path to the training data
+    - stem_type: str, type of stem to split (e.g. vocals, drums, bass, midrange)
+    - level: int, level of wavelet decomposition
+    - batch_size: int, size of the batch
+    - max_songs: int, maximum number of songs to use
+    - max_samples_per_song: int, maximum number of samples per song
+    
+    return: 
+    - tf.data.Dataset, batched wavelet data
+    '''
+    
+
+    # find all songs in the training data
+    songs = os.listdir(path_to_training + stem_type)
+
+    # limit the number of songs -- choose randomly
+    if len(songs) > max_songs:
+        songs = np.random.choice(songs, max_songs, replace=False)
+
+
+    # generate pairs for each song
+    y_train = []
+    y_true = []
+    shape = None
+    for song in songs:#tqdm.tqdm(songs, desc=f"Generating Wavelet Batch: (level = {level}, batch_size = {batch_size}, max_songs = {max_songs}, max_samples_per_song = {max_samples_per_song})", total=len(songs), leave=True):
+        
+        # Make sure song does not have bleed
+        song_name = song.split('/')[-1]
+        if song_name in BLEED_SONGS:
+            continue
+        
+        
+        # generate pairs for each song
+        path_to_song = path_to_training + stem_type + '/' + song 
+        if path_to_song[-1] != '/':
+            path_to_song += '/'
+
+        ## check if the song is a directory
+        if not os.path.isdir(path_to_song):
+            continue
+
+        ## check that song has y_train and y_true folders for the stem type
+        if not os.path.isdir(path_to_song + 'y_train/') or not os.path.isdir(path_to_song + 'y_true/'):
+            continue
+
+        train, true, shape = generate_pairs(path_to_song, stem_type, level, max_songs_per_stem=max_samples_per_song, diff=diff)
+        
+
+        # add to the list
+        y_train.append(train)
+        y_true.append(true)
+
+    # filter out empty samples or samples that have a mismatch in shape
+    valid_indices = []
+
+
+    for i in range(len(y_train)):
+        if len(y_train[i]) == 0 or len(y_true[i]) == 0:
+            continue
+        if len(y_train[i].shape) != 3:
+            continue
+        valid_indices.append(i)
+        
+    # valid_indices = np.random.choice(valid_indices, len(valid_indices), replace=False)
+
+    y_train = [y_train[i] for i in valid_indices]
+    y_true = [y_true[i] for i in valid_indices]
+
+
+    y_train = np.concatenate(y_train)
+    y_true = np.concatenate(y_true)
+    
+    y_train, y_true = downcoef_audio(y_train, y_true, shape, level)
+    
+    
+    # UNCOMMENT THIS LINE AND COMMENT THE LINE BELOW TO RETURN THE TENSORS FOR DEBUGGING
+    return y_train, y_true, shape 
